b712ed64e4b23b02d1dc7f1aa124dd2fe12dab29
Sebastian Olivera <88690503+SebastianOlivera@users.noreply.github.com>
2022-08-31 19:26:25 -0300

    Parte 1

--- src/Lista.java
+++ src/Lista.java
@@ -1,3 +1,4 @@
+
 public class Lista<T> implements ILista<T> {
 
     private Nodo<T> primero;
@@ -6,6 +7,55 @@ public class Lista<T> implements ILista<T> {
         primero = null;
     }
 
-
     // implementar los metodos indicados en la interfaz
+    @Override
+    public void insertar(Nodo<T> nodo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Nodo<T> buscar(Comparable clave) {
+        if (esVacia()) {
+            return null;
+        } else {
+            Nodo<T> aux = primero;
+            while (aux != null) {
+                if (aux.getEtiqueta().equals(clave)) {
+                    return aux;
+                }
+                aux = aux.getSiguiente();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean eliminar(Comparable clave) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String imprimir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String imprimir(String separador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int cantElementos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean esVacia() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setPrimero(Nodo<T> unNodo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
